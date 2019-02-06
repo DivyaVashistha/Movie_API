a6d2d25fa7cd3b3d82efd8e7eb06f2b0e1593703
Divya Vashistha <divyavashishtha23@gmail.com>
2019-02-06 15:46:08 +0530

    api dev

---
+++ MovieAPI/Controllers/Api/MovieController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MovieAPI.Models;
+
+namespace MovieAPI.Controllers.api
+{
+    public class MovieController : ApiController
+    {
+        ApplicationDbContext _context;
+        public MovieController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+public IEnumerable<Movie> GetMovies()
+        {
+            return _context.Movie.ToList();
+        }
+    }
+}
